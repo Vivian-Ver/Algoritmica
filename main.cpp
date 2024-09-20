250acae6f62f3a9ef49e4549755a8692b64f08fa
Vivian-Ver <24020096@uadeo.mx>
2024-09-20 13:44:00 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <conio.h>
 
 
-main()
+int main()
 {
-   printf("Hola Mundo");
+    int dia, mes, anio, edad;
+    printf ("Dame tu dia de nacimiento:");
+    scanf ("%d", &dia);
+
+    printf ("Dame tu mes de nacimiento:");
+    scanf ("%d", &mes);
+
+    printf("Dame tu anio de nacimiento:");
+    scanf("%d", &anio);
+
+    edad=2024-anio;
+    printf("Tu edad es: %d", edad);
+
+    getch();
 }
