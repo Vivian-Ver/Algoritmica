c9c2e15b05711d4c03da4b0d95cb33c66087f5c0
Vivian-Ver <24020096@uadeo.mx>
2024-10-03 20:48:55 -0700

    Add files via upload

---
+++ parcial1/edadios/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float an, ac, ed;
+
+    cout << "Anio actual: ";
+    cin >> ac;
+
+    cout << "Anio de Nacimiento: ";
+    cin >> an;
+
+    ed= ac - an;
+
+    cout << "Edad: " << ed << endl;
+
+}
---
+++ parcial1/fruta/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char fruta[20];
+    cout << "Introduzca su fruta favortita";
+    cin >> fruta;
+
+    cout << "Su fruta favorita es: " << fruta << endl;
+    return 0;
+}
